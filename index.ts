1a70cbc3c3491a98c1c45c8e1b1df1d5427a3f22
drimerdev <166228817+StabletulyMigDev@users.noreply.github.com>
2024-05-12 15:32:50 -0300

    add base

---
+++ index.ts
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById('loginForm') as HTMLFormElement;
+  
+    loginForm.addEventListener('submit', function (event) {
+      event.preventDefault(); // prevent form submission
+  
+      // You can add your login logic here
+      const username = (document.getElementById('username') as HTMLInputElement).value;
+      const password = (document.getElementById('password') as HTMLInputElement).value;
+  
+      // Load users from JSON file
+      fetch('users.json')
+        .then(response => response.json())
+        .then(users => {
+          // Check if the entered credentials match any user in the JSON
+          const user = users.find((user: any) => user.username === username && user.password === password);
+          
+          if (user) {
+            // Redirect to another page after successful login
+            window.location.href = './files/indexing.html';
+          } else {
+            alert('Invalid username or password.');
+          }
+        })
+        .catch(error => {
+          console.error('Error loading users:', error);
+          alert('Error occurred while loading users. Please try again later.');
+        });
+    });
+  });
+  
